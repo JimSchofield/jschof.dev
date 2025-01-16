d1b365503bbcc86c9b88c3bb53e0dd07598662ca
JimSchofield <23246869+JimSchofield@users.noreply.github.com>
2025-01-16 12:37:31 -0600

    Removing dynamic gradient as there is a flash of gradient change on page load

--- src/index.js
+++ src/index.js
@@ -5,12 +5,3 @@ import MobileMenu from './js/mobile-menu.js';
 import './js/baseline-status.min.js';
 
 new MobileMenu('#mobile-menu');
-
-(function setRandomGradientAngle() {
-  const [rangeBeginning, rangeEnd] = [30,144]
-  const gradientAngle = Math.floor(Math.random() * (rangeEnd - rangeBeginning) + rangeBeginning);
-
-  const root = document.documentElement;
-  root.style.setProperty('--gradient-angle', gradientAngle + 'deg');
-})();
-

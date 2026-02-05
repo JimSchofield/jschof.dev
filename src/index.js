8ad1bfa5621e00688f400911e6d9d4e454374480
JimSchofield <23246869+JimSchofield@users.noreply.github.com>
2026-02-05 12:40:46 -0600

    Post: you might not need an abstraction

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './js/play-ground.js';
 import './js/code-highlight.js';
 import './js/quick-search.js';
 import './js/reading-list.js';
+import './js/repl-playground.js';
 import './js/heading-links.js';
 import MobileMenu from './js/mobile-menu.js';
 import './js/baseline-status.min.js';
